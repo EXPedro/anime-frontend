be6a0c989ed81bfea10a493b343dd23c19152b0f
EXPedro <pedroelisio555@gmail.com>
2021-11-08 20:47:26 -0300

    Create sort arrow.

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,6 +5,9 @@
                 menu
             </span>
                 <h1>Lista de animes</h1>
+            <span class="material-icons">
+                expand_more
+            </span>   
         </div>
     <div class="rodape"></div>
   </div>
