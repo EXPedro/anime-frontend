9b74888ab0cc0e753f2357375f548a9db5b2523e
EXPedro <pedroelisio555@gmail.com>
2021-11-08 21:34:09 -0300

    Update buttons.

--- src/components/buttons.vue
+++ src/components/buttons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class= "buttons" >                
         <button type="button" class="botao"> Update </button>
-        <button type="button" class="botao"> + 1 </button>
+        <button type="button" class="botao"> +1 </button>
     </div>
 </template>
 <script>
